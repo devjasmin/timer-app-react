b7ae5559ba6402f1c3e253f96f59593872ad3cf0
Jasmin <j.longa@gmx.ch>
2026-06-23 19:57:44 +0200

    ohne "s"

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -2,5 +2,5 @@ export function formatTime(seconds: number) {
   /*const mins = Math.floor(seconds / 60);*/
   const secs = seconds % 60;
 
-  return `${secs.toFixed(3).padStart(5, "0")}s`;
+  return `${secs.toFixed(3).padStart(5, "0")}`;
 }
